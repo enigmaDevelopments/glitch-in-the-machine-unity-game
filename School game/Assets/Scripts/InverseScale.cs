f07951cbddce70038442f3110373f95e69b75634
enigmaDevelopments <enigmarezac@gmail.com>
2024-04-24 18:29:40 -0400

    Update InverseScale.cs
    Fixes error with the inversor script that made the player character warp when standing on powered walls

--- School game/Assets/Scripts/InverseScale.cs
+++ School game/Assets/Scripts/InverseScale.cs
@@ -7,6 +7,6 @@ public class InverseScale : MonoBehaviour
     public Transform wall;
     void Update()
     {
-        transform.localScale = new Vector2(1, Mathf.Pow(wall.transform.localScale.y * .8f,-1));
+        transform.localScale = new Vector2(1, Mathf.Pow(wall.transform.localScale.y,-1));
     }
 }
