5431709b8afda2aeb949f45a683f954b1f76f9da
enigmaDevelopments <enigmarezac@gmail.com>
2024-04-24 18:35:33 -0400

    remove unused .sln file

--- School game/Assets/Scripts/Player/ParentDeadPlayerToMovable.cs
+++ School game/Assets/Scripts/Player/ParentDeadPlayerToMovable.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Networking;
 using UnityEngine;
-using System.Threading;
 namespace Player {
     public class ParentDeadPlayerToMovable : ParentPlayerToMovable
     {
