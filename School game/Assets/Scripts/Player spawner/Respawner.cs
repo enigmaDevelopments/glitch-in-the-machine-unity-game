373699527a471c983e483331cee30cbe3b05ce8b
enigmaDevelopments <enigmarezac@gmail.com>
2024-04-25 14:50:49 -0400

    create check point
    created new object checkpoint

--- School game/Assets/Scripts/Player spawner/Respawner.cs
+++ School game/Assets/Scripts/Player spawner/Respawner.cs
@@ -1,3 +1,4 @@
+using Style;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,15 @@ namespace Spawner
 {
     public class Respawner : PlayerSpawner
     {
+        public Sprite on;
         private bool active = false;
         void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.layer == 3)
+            {
                 active = true;
+                SpriteChanger.changeSprite(on,gameObject);
+            }
         }
         override public void spawn()
         {
